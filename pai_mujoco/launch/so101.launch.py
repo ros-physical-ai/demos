617ce7ba67e7b99edb33f2215140213ad5b5c443
Sebastian Castro <sebas.a.castro@gmail.com>
2025-12-14 12:22:23 -0500

    Basic Mujoco setup

---
+++ pai_mujoco/launch/so101.launch.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue, ParameterFile
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("pai_mujoco"),
+                    "urdf",
+                    "so_arm101.urdf.xacro",
+                ]
+            ),
+        ]
+    )
+
+    robot_description = {"robot_description": ParameterValue(value=robot_description_content, value_type=str)}
+
+    controller_parameters = ParameterFile(
+        PathJoinSubstitution([FindPackageShare("pai_mujoco"), "config", "ros2_controllers.yaml"]),
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[
+            robot_description,
+            {"use_sim_time": True},
+        ],
+    )
+
+    control_node = Node(
+        package="mujoco_ros2_simulation",
+        executable="ros2_control_node",
+        output="both",
+        parameters=[
+            {"use_sim_time": True},
+            controller_parameters,
+        ],
+    )
+
+    spawn_joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        name="spawn_joint_state_broadcaster",
+        arguments=[
+            "joint_state_broadcaster",
+        ],
+        output="both",
+    )
+
+    spawn_jtc = Node(
+        package="controller_manager",
+        executable="spawner",
+        name="spawn_joint_trajectory_controller",
+        arguments=[
+            "joint_trajectory_controller",
+        ],
+        output="both",
+    )
+
+    spawn_gripper_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        name="spawn_gripper_controller",
+        arguments=[
+            "gripper_controller",
+        ],
+        output="both",
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("pai_mujoco"), "rviz", "so_arm.rviz"]
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    return LaunchDescription(
+        [
+            robot_state_publisher_node,
+            control_node,
+            spawn_joint_state_broadcaster,
+            spawn_jtc,
+            spawn_gripper_controller,
+            rviz_node,
+        ]
+    )
---
+++ pai_mujoco/urdf/README.md
@@ -0,0 +1,3 @@
+NOTE: This is just bringing the assets in from source temporarily.
+
+Pull requests like https://github.com/JafarAbdi/ros2_so_arm100/pull/24 need to be merged into the `ros2_so_arm100` repo before we can consolidate.
